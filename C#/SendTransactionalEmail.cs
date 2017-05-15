c009fad33a97062b09ee8d1adf1791e17124fbb5
Erick Pece <erick@pece.me>
2017-05-15 14:51:59 -0400

    Added SendTransactionalEmail example

---
+++ C#/SendTransactionalEmail.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+namespace JangoMailAPIExample
+{
+    /// <summary>
+    /// JangoMail API Example Code
+    /// To use this example, add a Service Reference to http://api.jangomail.com/api.asmx?WSDL as JangoMailService.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Sample code to execute SendTransactionalEmail method
+        /// </summary>
+        static void Main(string[] args)
+        {
+            JangoMailService.JangoMailSoapClient client = new JangoMailService.JangoMailSoapClient("JangoMailSoap");
+
+            string response = client.SendTransactionalEmail(
+                "Username",
+                "Password",
+                "FromEmail",
+                "FromName",
+                "ToEmailAddress",
+                "Subject",
+                "MessagePlain",
+                "MessageHTML",
+                "Options");
+                
+            Console.WriteLine(response);
+
+            Console.ReadKey();
+        }
+    }
+}
